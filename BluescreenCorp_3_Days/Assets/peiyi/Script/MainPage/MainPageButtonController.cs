c881365992c0c90ca681544f61e352040957fe98
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2020-11-22 21:47:18 +0800

    map selection and main page

---
+++ BluescreenCorp_3_Days/Assets/peiyi/Script/MainPage/MainPageButtonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainPageButtonController : MonoBehaviour
+{
+    public GameObject settingPanel;
+    public Image FadeInFadeOut;
+
+    private void Start()
+    {
+        settingPanel.SetActive(false);
+    }
+
+    public void OnClickPlay()
+    {
+        StartCoroutine(changeScene());
+    }
+
+    public void OnClickSetting()
+    {
+        settingPanel.SetActive(true); 
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator changeScene()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(1);
+    }
+
+    
+}
