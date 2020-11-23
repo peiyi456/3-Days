21caef8ff01f470147b76acf32cc411ebc2a948b
William8500 <William8500@users.noreply.github.com>
2020-11-23 18:12:34 +0800

    Revert "Revert "map selection and main page""
    This reverts commit 94b4343a9a7216624ddfe48ce4ebc1e630e0ab78.

---
+++ BluescreenCorp_3_Days/Assets/peiyi/Script/Pickup&Inventory/Items/WoodSword.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodSword : MonoBehaviour, IInventoryItem
+{
+    public string Name
+    {
+        get
+        {
+            return "WoodSword";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public void OnPickup()
+    {
+        // TODO: Add logic what happens when axe is picked up by player
+        gameObject.SetActive(false);
+    }
+}
