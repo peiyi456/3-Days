51a64602e5dfe64fc6a5a9679e8ebe0d1d6fb64f
William8500 <William8500@users.noreply.github.com>
2020-12-07 08:17:05 +0800

    Some Fixed

--- BluescreenCorp_3_Days/Assets/William/UILayering.cs
+++ BluescreenCorp_3_Days/Assets/William/UILayering.cs
@@ -28,80 +28,126 @@ public class UILayering : MonoBehaviour
     int pageAnimalsSortingNumber;
     int pageInformationSortingNumber;
 
+    bool isTriggerChangingNumber;
     void Start()
     {
-        pageStatus.GetComponent<Renderer>().sortingOrder = pageStatusSortingNumber;
-        pageInventory.GetComponent<Renderer>().sortingOrder = pageInventorySortingNumber;
-        pageCrafting.GetComponent<Renderer>().sortingOrder = pageCraftingSortingNumber;
-        pageAnimals.GetComponent<Renderer>().sortingOrder = pageAnimalsSortingNumber;
-        pageInformation.GetComponent<Renderer>().sortingOrder = pageInformationSortingNumber;
-
-        pageStatusSortingNumber = 1;
+        pageStatusSortingNumber = 6;
         pageInventorySortingNumber = 3;
         pageCraftingSortingNumber = 4;
         pageAnimalsSortingNumber = 5;
-        pageInformationSortingNumber = 6;
-    }
+        pageInformationSortingNumber = 1;
+        //initialise staring layer sequece
 
-    void OnMouseDown()
+        pageStatus.GetComponent<SpriteRenderer>().sortingOrder = pageStatusSortingNumber;
+        pageInventory.GetComponent<SpriteRenderer>().sortingOrder = pageInventorySortingNumber;
+        pageCrafting.GetComponent<SpriteRenderer>().sortingOrder = pageCraftingSortingNumber;
+        pageAnimals.GetComponent<SpriteRenderer>().sortingOrder = pageAnimalsSortingNumber;
+        pageInformation.GetComponent<SpriteRenderer>().sortingOrder = pageInformationSortingNumber;
+        // put in the layer sequence into game object sprite renderer
+
+        isTriggerChangingNumber = false;
+    }
+    void getSpiriteRendererUpdate()
+    {
+        pageStatus.GetComponent<SpriteRenderer>().sortingOrder = pageStatusSortingNumber;
+        pageInventory.GetComponent<SpriteRenderer>().sortingOrder = pageInventorySortingNumber;
+        pageCrafting.GetComponent<SpriteRenderer>().sortingOrder = pageCraftingSortingNumber;
+        pageAnimals.GetComponent<SpriteRenderer>().sortingOrder = pageAnimalsSortingNumber;
+        pageInformation.GetComponent<SpriteRenderer>().sortingOrder = pageInformationSortingNumber;
+    }
+    void TriggerChanging()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isTriggerChangingNumber = true;
+            {
+                if (isTriggerChangingNumber == true)
+                {
+                    if (pageStatusButton)
+                    {
+                        pageStatusSortingNumber = 6;
+                        pageInventorySortingNumber = 3;
+                        pageCraftingSortingNumber = 4;
+                        pageAnimalsSortingNumber = 5;
+                        pageInformationSortingNumber = 1;
+                        Debug.Log("OI");
+                        isTriggerChangingNumber = false;
+                    }
+                    if (pageInventoryButton)
+                    {
+                        pageStatusSortingNumber = 3;
+                        pageInventorySortingNumber = 6;
+                        pageCraftingSortingNumber = 4;
+                        pageAnimalsSortingNumber = 5;
+                        pageInformationSortingNumber = 1;
+                        Debug.Log("Bro");
+                        isTriggerChangingNumber = false;
+                    }
+                    if (pageCraftingButton)
+                    {
+                        pageStatusSortingNumber = 2;
+                        pageInventorySortingNumber = 3;
+                        pageCraftingSortingNumber = 6;
+                        pageAnimalsSortingNumber = 5;
+                        pageInformationSortingNumber = 1;
+                        isTriggerChangingNumber = false;
+                    }
+                    if (pageAnimalsButton)
+                    {
+                        pageStatusSortingNumber = 2;
+                        pageInventorySortingNumber = 3;
+                        pageCraftingSortingNumber = 4;
+                        pageAnimalsSortingNumber = 6;
+                        pageInformationSortingNumber = 1;
+                        isTriggerChangingNumber = false;
+                    }
+                    if (pageInformationButton)
+                    {
+                        pageStatusSortingNumber = 2;
+                        pageInventorySortingNumber = 3;
+                        pageCraftingSortingNumber = 4;
+                        pageAnimalsSortingNumber = 5;
+                        pageInformationSortingNumber = 6;
+                        isTriggerChangingNumber = false;
+                    }
+                }
+            }
+        }
+      
+    }
+    void OnMouseEnter()
     {
         if (pageStatusButton)
         {
-            pageStatusSortingNumber = 1;
-            pageInventorySortingNumber = 3;
-            pageCraftingSortingNumber = 4;
-            pageAnimalsSortingNumber = 5;
-            pageInformationSortingNumber = 6;
+        
 
         }
         if (pageInventoryButton)
         {
-            pageStatusSortingNumber = 3;
-            pageInventorySortingNumber = 1;
-            pageCraftingSortingNumber = 4;
-            pageAnimalsSortingNumber = 5;
-            pageInformationSortingNumber = 6;
+           
         }
         if (pageCraftingButton)
         {
-            pageStatusSortingNumber = 2;
-            pageInventorySortingNumber = 3;
-            pageCraftingSortingNumber = 1;
-            pageAnimalsSortingNumber = 5;
-            pageInformationSortingNumber = 6;
+            
         }
         if (pageAnimalsButton)
         {
-            pageStatusSortingNumber = 2;
-            pageInventorySortingNumber = 3;
-            pageCraftingSortingNumber = 4;
-            pageAnimalsSortingNumber = 1;
-            pageInformationSortingNumber = 6;
+           
         }
         if (pageInformationButton)
-        {
-            pageStatusSortingNumber = 2;
-            pageInventorySortingNumber = 3;
-            pageCraftingSortingNumber = 4;
-            pageAnimalsSortingNumber = 5;
-            pageInformationSortingNumber = 1;
-        }
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (pageAnimalsSortingNumber == 1)
         {
             
         }
     }
     void Update()
     {
-
+        TriggerChanging();
+        Debug.Log(pageStatusSortingNumber);
     }
 
     void LateUpdate()
     {
-        OnMouseDown();
+        getSpiriteRendererUpdate();
     }
   
 }
