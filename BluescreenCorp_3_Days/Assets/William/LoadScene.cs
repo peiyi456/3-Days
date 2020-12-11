83ee458a2ddcc3b8c74087959e42226a14e09465
William8500 <William8500@users.noreply.github.com>
2020-12-12 00:42:17 +0800

    Will

---
+++ BluescreenCorp_3_Days/Assets/William/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public float boundryMinX = -55.0f;
+    public float boundryMaxX = 29.0f;
+    public float boundryMinY = -33.0f;
+    public float boundryMaxY = 35.0f;
+
+    // Start is called before the first frame update
+    public void SceneLV()
+    {
+       
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneLV();
+    }
+}
