5bd5abd2ceaa8082ac6d137834f76647ec18b83b
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2021-07-30 11:23:44 +0800

    update 2

---
+++ BluescreenCorp_3_Days/Assets/peiyi/TestingScene/Scripts/ShortcutKeyFunction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortcutKeyFunction : MonoBehaviour
+{
+    //[SerializeField] KeyCode keyCode;
+    //[SerializeField] InventoryButton buttons;
+    ItemContainer inventory;
+    bool press;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        press = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inventory = GameManager.instance.inventoryContainer;
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            inventory.RemoveItem(inventory.slots[9].item, 1);
+        }
+        
+    }
+}
