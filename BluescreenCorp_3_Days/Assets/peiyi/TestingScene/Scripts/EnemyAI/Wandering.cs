065eb9cf450375eda0fe599275fd3ee3d451e469
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2021-08-31 03:53:26 +0800

    Animal Wandering AI Done

---
+++ BluescreenCorp_3_Days/Assets/peiyi/TestingScene/Scripts/EnemyAI/Wandering.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Wandering : MonoBehaviour
+{
+    NavMeshAgent navMesh;
+    public Transform WanderingArea;
+    Vector3 Destination;
+
+
+    bool isArrive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        navMesh.updateRotation = false;
+        navMesh.updateUpAxis = false;
+        //navMesh.SetDestination(WanderingArea.position);
+    }
+
+    private void FixedUpdate()
+    {
+        //if(isArrive)
+        //{
+
+        //}
+        //else
+        //{
+
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isArrive)
+        {
+            Vector2 randomInCiecle = new Vector2(WanderingArea.position.x, WanderingArea.position.y) + Random.insideUnitCircle * 10f;
+            Destination = new Vector3(randomInCiecle.x, randomInCiecle.y, 0f);
+
+            isArrive = false;
+            navMesh.SetDestination(Destination);
+
+            StartCoroutine(WanderingFunc());
+        }
+        float x = Destination.x - transform.position.x;
+        if (x >= 0)
+        {
+            //transform.localScale = new Vector3(0.4151863f, 0.4151863f, 0.4151863f);
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<SpriteRenderer>().flipY = false;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<SpriteRenderer>().flipY = false;
+        }
+    }
+
+    IEnumerator WanderingFunc()
+    {
+        float dist = Vector3.Distance(this.transform.position, Destination);
+
+        while(dist >= 0.1f)
+        {
+            dist = Vector3.Distance(this.transform.position, Destination);
+            yield return null;
+        }
+        yield return new WaitForSeconds(3f);
+        isArrive = true;
+    }
+
+
+}
