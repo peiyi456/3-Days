89faa03486d9706ab916244492f6a7b8f3338943
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2021-09-09 20:27:37 +0800

    add help page

---
+++ BluescreenCorp_3_Days/Assets/William/HelpButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpButton : MonoBehaviour
+{
+    public GameObject gettingComponent;
+    public GameObject changeColour;
+    void Start()
+    {
+        changeColour.gameObject.SetActive(false);
+    }
+    public void Trigger()
+    {
+        UILayering TriggerChangingInformation = gettingComponent.GetComponent<UILayering>();
+        TriggerChangingInformation.isTriggerInformation = true;
+    }
+    void OnMouseEnter()
+    {
+        changeColour.gameObject.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        changeColour.gameObject.SetActive(false);
+    }
+}
